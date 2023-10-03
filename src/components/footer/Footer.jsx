9194f1832d27cc4ba8bd72efa3cdc18495245935
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-10-03 11:32:41 +0530

    Add footer and Navbar styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
 	console.log("Hello");
 	return (
-		<div>
+		<div className={styles.container}>
 			<div>2023 DC-Kaushalya. All rights reserved</div>
 			<div></div>
 		</div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import styles from "./navbar.module.css";
 
 const links = [
 	{
@@ -37,15 +38,18 @@ const links = [
 
 const Navbar = () => {
 	return (
-		<div>
-			<Link href="/">DC-Kaushalya</Link>
-			<div>
+		<div className={styles.container}>
+			<Link href="/" className={styles.logo}>
+				DC-Kaushalya
+			</Link>
+			<div className={styles.links}>
 				{links.map((link) => (
-					<Link key={link.id} href={link.url}>
+					<Link key={link.id} href={link.url} className={styles.link}>
 						{link.title}
 					</Link>
 				))}
 				<button
+					className={styles.logout}
 					onClick={() => {
 						console.log("logged out");
 					}}
