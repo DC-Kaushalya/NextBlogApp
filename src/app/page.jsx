0542f9e197ebbc648046a8d5d1e497c835d6923a
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-10-05 22:37:47 +0530

    Add internal image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,7 @@ import Hero from "public/hero.jpg";
 export default function Home() {
 	return (
 		<div>
-			<Image
-				width={500}
-				height={500}
-				src="https://images.pexels.com/photos/1005417/pexels-photo-1005417.jpeg?auto=compress&cs=tinysrgb&w=600"
-				alt=""
-				className={styles.img}
-			/>
+			<Image src={Hero} alt="" className={styles.img} />
 		</div>
 	);
 }
