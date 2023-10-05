b106c738ad6305a1ca1bc2bc30a63544f0325fe0
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-10-05 21:49:58 +0530

    Modify footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
 	console.log("Hello");
 	return (
 		<div className={styles.container}>
 			<div>2023 DC-Kaushalya. All rights reserved</div>
-			<div></div>
+			<div className={styles.social}>
+				<Image
+					src="/facebook.png"
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt="DC-Kaushalya facebook"
+				/>
+				<Image
+					src="/youtube.png"
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt="DC-Kaushalya youtube"
+				/>
+				<Image
+					src="/instagram.png"
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt="DC-Kaushalya instagram"
+				/>
+				<Image
+					src="/gmail.png"
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt="DC-Kaushalya gmail"
+				/>
+			</div>
 		</div>
 	);
 };
