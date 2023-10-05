4a69d6afb8a58fc281fb270f13e30389b99924e7
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-10-05 23:58:48 +0530

    Create home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,18 @@ import Hero from "public/hero.jpg";
 
 export default function Home() {
 	return (
-		<div>
+		<div className={styles.container}>
+			<div className={styles.item}>
+				<h1 className={styles.title}>
+					Better design for your digital products
+				</h1>
+				<p className={styles.desc}>
+					Turning your Idea into Reality. We bring together the team
+					from the global graphic industry.
+				</p>
+				<button className={styles.button}>See our Works</button>
+			</div>
+			<div className={styles.item}></div>
 			<Image src={Hero} alt="" className={styles.img} />
 		</div>
 	);
