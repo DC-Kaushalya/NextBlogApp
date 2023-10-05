13a46acdfb51a35fbc8bf5bac1916f7bef8d97d3
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-10-05 22:36:41 +0530

    Add external image using next.config.js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.jpg";
 
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div>
+			<Image
+				width={500}
+				height={500}
+				src="https://images.pexels.com/photos/1005417/pexels-photo-1005417.jpeg?auto=compress&cs=tinysrgb&w=600"
+				alt=""
+				className={styles.img}
+			/>
+		</div>
+	);
 }
