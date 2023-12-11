860246fd15844bd9ce4ce6f869ae4bd4f7815aa0
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-12-11 19:44:02 +0530

    Change images

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
 			<div className={styles.content}>
 				<div className={styles.imgContainer}>
 					<Image
-						src="/contact.jpg"
+						src="/contact.png"
 						alt=""
 						fill={true}
 						className={styles.image}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Hero from "public/hero.jpg";
+import Hero from "public/hero.png";
 import Button from "@/components/Buttons/Button";
 
 export default function Home() {
