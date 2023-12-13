f34b274df257bd797b28e0b419360849f50bf9d5
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-12-13 13:30:35 +0530

    Create blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const BlogPost = () => {
-	return <div>BlogPost</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.top}>
+				<div className={styles.info}>
+					<h1 className={styles.title}>title</h1>
+					<p className={styles.desc}>
+						Lorem ipsum dolor sit amet consectetur, adipisicing
+						elit. Illo, dicta magnam incidunt velit debitis vero
+						eveniet distinctio facere temporibus obcaecati nobis
+						suscipit aperiam voluptatem eligendi hic aut vitae,
+						nesciunt nihil.
+					</p>
+					<div className={styles.author}>
+						<Image
+							src="https://images.pexels.com/photos/7933505/pexels-photo-7933505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+							alt=""
+							width={40}
+							height={40}
+							className={styles.avatar}
+						/>
+						<span className={styles.username}>User</span>
+					</div>
+				</div>
+				<div className={styles.imageContainer}>
+					<Image
+						src="https://images.pexels.com/photos/7933505/pexels-photo-7933505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+						alt=""
+						fill={true}
+						className={styles.image}
+					/>
+				</div>
+			</div>
+			<div className={styles.content}>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Neque sint dolorum, aut perspiciatis debitis, enim
+					consequatur modi molestiae excepturi cupiditate mollitia
+					minus provident nemo fugiat culpa aliquid illo eaque
+					voluptatum.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default BlogPost;
