11a8c942653110a7b7e56a1b4396285e51e04279
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-12-14 19:22:43 +0530

    Add categories

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,54 +2,42 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Buttons/Button";
 import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+	const data = items[cat];
+
+	if (data) {
+		return data;
+	}
+
+	return notFound();
+};
 
 const Category = ({ params }) => {
-	console.log(params);
+	const data = getData(params.category);
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.catTitle}>{params.category}</h1>
-			<div className={styles.item}>
-				<div className={styles.content}>
-					<h1 className={styles.title}>Creative Portfolio</h1>
-					<p className={styles.desc}>
-						Lorem ipsum, dolor sit amet consectetur adipisicing
-						elit. Ducimus quo totam nobis sint, veniam distinctio
-						omnis tenetur. Nemo, exercitationem alias asperiores,
-						eius omnis non natus numquam assumenda totam eveniet
-						praesentium.
-					</p>
-					<Button text="See More" url="#" />
+
+			{data.map((item) => (
+				<div className={styles.item} key={item.id}>
+					<div className={styles.content}>
+						<h1 className={styles.title}>{item.title}</h1>
+						<p className={styles.desc}>{item.desc}</p>
+						<Button text="See More" url="#" />
+					</div>
+					<div className={styles.imgContainer}>
+						<Image
+							className={styles.img}
+							fill={true}
+							src={item.image}
+							alt=""
+						/>
+					</div>
 				</div>
-				<div className={styles.imgContainer}>
-					<Image
-						className={styles.img}
-						fill={true}
-						src="https://images.pexels.com/photos/7933505/pexels-photo-7933505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-						alt=""
-					/>
-				</div>
-			</div>
-			<div className={styles.item}>
-				<div className={styles.content}>
-					<h1 className={styles.title}>Minimal Signal Product</h1>
-					<p className={styles.desc}>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Eveniet accusamus fugit magni ipsum est aut quos ex.
-						Deleniti nam, asperiores blanditiis delectus
-						voluptatibus officia accusamus eum culpa distinctio
-						fugit earum.
-					</p>
-					<Button text="See More" url="#" />
-				</div>
-				<div className={styles.imgContainer}>
-					<Image
-						className={styles.img}
-						fill={true}
-						src="https://images.pexels.com/photos/7933505/pexels-photo-7933505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-						alt=""
-					/>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
