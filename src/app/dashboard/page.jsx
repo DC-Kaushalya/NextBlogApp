ce9cacd0cd70ae54e8fa512a80c299e93ba468f0
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-12-16 02:18:35 +0530

    Add dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import useSWR from "swr";
 
 const Dashboard = () => {
@@ -32,11 +33,14 @@ const Dashboard = () => {
 	// 	getData();
 	// }, []);
 
+	const session = useSession();
 
+	const router = useRouter();
 
 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 	const { data, error, isLoading } = useSWR(
+		`/api/posts?username=${session?.data?.user.name}`,
 		fetcher
 	);
 
@@ -50,8 +54,80 @@ const Dashboard = () => {
 		router?.push("/dashboard/login");
 	}
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const title = e.target[0].value;
+		const desc = e.target[1].value;
+		const img = e.target[2].value;
+		const content = e.target[3].value;
+
+		try {
+			await fetch("/api/posts", {
+				method: "POST",
+				body: JSON.stringify({
+					username: session.data.user.name,
+					title,
+					desc,
+					img,
+					content,
+					username: session.data.user.name,
+				}),
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	if (session.status === "authenticated") {
-		return <div className={styles.container}>Dashboard</div>;
+		return (
+			<div className={styles.container}>
+				<div className={styles.post}>
+					{isLoading
+						? "loading"
+						: data?.map((post) => (
+								<div className={styles.post} key={post._id}>
+									<div className={styles.imgContainer}>
+										<Image
+											src={post.img}
+											alt=""
+											width={200}
+											height={100}
+										/>
+									</div>
+									<h2 className={styles.postTittle}>
+										{post.title}
+									</h2>
+									<span className={styles.delete}>X</span>
+								</div>
+						  ))}
+				</div>
+				<form className={styles.new} onSubmit={handleSubmit}>
+					<h1>Add New Post</h1>
+					<input
+						type="text"
+						placeholder="Title"
+						className={styles.input}
+					/>
+					<input
+						type="text"
+						placeholder="Desc"
+						className={styles.input}
+					/>
+					<input
+						type="text"
+						placeholder="Image"
+						className={styles.input}
+					/>
+					<textarea
+						placeholder="Content"
+						className={styles.textArea}
+						cols="30"
+						rows="10"
+					></textarea>
+					<button className={styles.button}>Send</button>
+				</form>
+			</div>
+		);
 	}
 };
 
